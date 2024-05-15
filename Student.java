1ac4b2e55f1436f5d0dcf166f68bc401cfe5f37c
Zahra Azizi <148708816+ZahraAziziGit@users.noreply.github.com>
2024-05-15 14:24:56 +0330

    add: mini project

---
+++ Student.java
@@ -0,0 +1,67 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Student {
+	private String firstName;
+	private String lastName;
+	private int id;
+	private String password;
+	private int numberOfCourses;
+	private int numberOfUnits;
+	private int numberOfRegistrationUnits;
+	private List<Course> listOfCourses = new ArrayList<>();
+	private double totalAverage;
+	private double currentTermAverage;
+	Map<Double, Double> marks = new HashMap<>();
+
+	public Student(String firstName, String lastName, int id){
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.id = id;
+	}
+
+	public void printListOfCourses(){
+		for (int i = 0; i < listOfCourses.size(); i++) {
+			Course course = listOfCourses.get(i);
+			System.out.println(i + ". " + course.getName());
+		}
+	}
+
+	public void calculateTotalAverage(){
+		double sumMarks = 0;
+		double sumUnits = 0;
+		for (Map.Entry<Double, Double> mark : marks.entrySet()) {
+			sumMarks += mark.getKey() * mark.getValue();
+			sumUnits += mark.getValue();
+		}
+		totalAverage = sumMarks / sumUnits;
+	}
+
+	public void printTotalAverage(){
+		calculateTotalAverage();
+		System.out.println(totalAverage);
+	}
+
+	public void printNumberOfRegistrationUnits(){
+		System.out.println(numberOfRegistrationUnits);
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+}
---
+++ Teacher.java
@@ -0,0 +1,58 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.time.LocalDate;
+
+public class Teacher {
+	private String teacherID;
+	private String firstName;
+	private String lastName;
+	private int numberOfCourses;
+	private List<Course> listOfCourses = new ArrayList<>();
+
+	public Teacher(String teacherID, String firstName, String lastName) {
+		this.teacherID = teacherID;
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	public void addStudent(Student student, Course course) {
+		course.addStudent(student);
+	}
+
+	public void removeStudent(Student student, Course course) {
+		course.removeStudent(student);
+	}
+
+	public void giveMark(Course course, Student student, double mark) {
+		student.marks.put(mark, course.getNumberOfUnits());
+		if (mark > course.getHighestMark()) {
+			course.setHighestMark(mark);
+		}
+	}
+
+	public void changeDeadline(Assignment assignment, LocalDate newDeadline) {
+		assignment.setDeadline(newDeadline);
+	}
+
+	public void addProject(Course course, Assignment assignment){
+		course.getListOfAssignments().add(assignment);
+	}
+	
+	public void removeProject(Course course, Assignment assignment){
+		course.getListOfAssignments().remove(assignment);
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public String getTeacherID() {
+		return teacherID;
+	}
+
+
+}
