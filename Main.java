7b0290accc5b1a3e2fa449f1b7d170659d464b41
Zahra Azizi <148708816+ZahraAziziGit@users.noreply.github.com>
2024-05-20 10:09:59 +0330

    fix: improved Main

--- Main.java
+++ Main.java
@@ -1,37 +1,72 @@
 import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) {
-		String RESET = "\u001B[0m";
-		String RED = "\u001B[31m";
-		String GREEN = "\u001B[32m";
-		String YELLOW = "\u001B[33m";
-		System.out.println("Greetings! To access the system, please choose your role.");
-		System.out.println("If you are an Admin, type Admin.");
-		System.out.println("If you are a Teacher, type Teacher.");
-		System.out.print("Enter your role: ");
+    public static void main(String[] args) {
+        //color codes
+        String RESET = "\u001B[0m";
+        String RED = "\u001B[31m";
+        String GREEN = "\u001B[32m";
+        String YELLOW = "\u001B[33m";
+        //first message
+        System.out.println("""
+                -----------------Greetings!-----------------
+                To access the system, please choose your role.
+                If you are an Admin, type "Admin".
+                If you are an Teacher, type "Teacher".""");
+        System.out.print("Enter your role: ");
 
-		Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-		while (true) {
-			String role = scanner.nextLine();
-			if ("Admin".equalsIgnoreCase(role)) {
-				System.out.print("\033[H\033[2J");
-				System.out.flush();
-				System.out.println("Welcome Admin!");
-				break;
-			} else if ("Teacher".equalsIgnoreCase(role)) {
-				System.out.print("\033[H\033[2J");
-				System.out.flush();
-				System.out.println("Welcome Teacher!");
-				break;
-			}
-			else {
-				System.out.print("\033[H\033[2J");
-				System.out.flush();
-				System.out.println(RED + "Invalid Role, Please Try Again." + RESET);
-				System.out.print("Enter your role: ");
-			}
-		}
-	}
+        String role = scanner.nextLine();
+        while (true) {
+            if ("Admin".equalsIgnoreCase(role)) {
+                System.out.print("\033[H\033[2J");
+                System.out.flush();
+                System.out.println(GREEN + "Welcome Admin!" + "\n" + RESET
+                        + "Chose from the list below:" + "\n" +
+                        "---------------------------------");
+                break;
+            } else if ("Teacher".equalsIgnoreCase(role)) {
+                System.out.print("\033[H\033[2J");
+                System.out.flush();
+                System.out.println(GREEN + "Welcome Teacher!" + "\n" + RESET
+                        + "Chose from the list below:" + "\n" +
+                        "---------------------------------");
+                break;
+            } else {
+                System.out.print("\033[H\033[2J");
+                System.out.flush();
+                System.out.println(RED + "Invalid Role, Please Try Again." + RESET);
+                System.out.print("Enter your role: ");
+            }
+        }
+        //Admin menu
+        if ("Admin".equalsIgnoreCase(role)) {
+            System.out.println("""
+                   
+                    """);
+        }
+
+
+        //Teacher menu
+        //به نظرم بعد از انتخاب عملکرد ایدی استاد رو دریافت کنیم
+        //می تونیم بعد از انتخاب role استاد و قبل از پیام
+        //خوش آمد هم دریافت کنیم.
+        if ("Teacher".equalsIgnoreCase(role)) {
+            System.out.println("""
+                    1. Add student
+                    2. remove student
+                    3. Give mark to a student
+                    4. Change an assignment deadline
+                    5. Add assignment
+                    6. Remove assignment
+                    """);
+        }
+        int action = scanner.nextInt();
+        switch (action) {
+            case 1:
+                System.out.println("adding student...");
+        }
+
+    }
 }
