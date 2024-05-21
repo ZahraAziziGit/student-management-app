0c97fb71dce3bd37ddb59c01a626f6554c838df9
zahra azizi <azizi.zahra.tehran@gmail.com>
2024-05-21 23:04:53 +0330

    feat: admin menu action 7-10

--- src/Main.java
+++ src/Main.java
@@ -88,12 +88,15 @@ public class Main {
 
             //variables
             String studentID, courseID, assignmentID, assignmentDeadline, teacherID,
-                    teacherFirstName, teacherLastName;
+                    teacherFirstName, teacherLastName, courseName, courseExamDate;
             double mark = -1;
-            LocalDate deadline = LocalDate.now();
+            int numberOfUnits = 0;
+            LocalDate deadline = LocalDate.now(), dateOfExam = LocalDate.now();
 
             switch (adminAction) {
                 case "1": //add student
+                    clear();
+                    System.out.println(YELLOW + "Adding student..." + RESET);
                     System.out.print("Enter Student ID: ");
                     studentID = scanner.nextLine();
                     System.out.print("Enter Course ID: ");
@@ -108,6 +111,8 @@ public class Main {
                     isAdminActionChosen = true;
                     break;
                 case "2": //remove student
+                    clear();
+                    System.out.println(YELLOW + "Removing student..." + RESET);
                     System.out.print("Enter Student ID: ");
                     studentID = scanner.nextLine();
                     System.out.print("Enter Course ID: ");
@@ -122,6 +127,8 @@ public class Main {
                     isAdminActionChosen = true;
                     break;
                 case "3": //give mark
+                    clear();
+                    System.out.println(YELLOW + "Giving mark to a student..." + RESET);
                     System.out.print("Enter Student ID: ");
                     studentID = scanner.nextLine();
                     System.out.print("Enter Course ID: ");
@@ -145,6 +152,8 @@ public class Main {
                     isAdminActionChosen = true;
                     break;
                 case "4": //change deadline
+                    clear();
+                    System.out.println(YELLOW + "Changing deadline..." + RESET);
                     System.out.print("Enter Assignment ID: ");
                     assignmentID = scanner.nextLine();
                     boolean isDeadlineCorrect = false;
@@ -166,6 +175,8 @@ public class Main {
                     isAdminActionChosen = true;
                     break;
                 case "5": //add assignment
+                    clear();
+                    System.out.println(YELLOW + "Adding assignment..." + RESET);
                     System.out.print("Enter Course ID: ");
                     courseID = scanner.nextLine();
                     System.out.print("Enter Assignment ID: ");
@@ -178,6 +189,8 @@ public class Main {
                     isAdminActionChosen = true;
                     break;
                 case "6": //remove assignment
+                    clear();
+                    System.out.println(YELLOW + "Removing student..." + RESET);
                     System.out.print("Enter Course ID: ");
                     courseID = scanner.nextLine();
                     System.out.print("Enter Assignment ID: ");
@@ -190,20 +203,86 @@ public class Main {
                     isAdminActionChosen = true;
                     break;
                 case "7": //add teacher
-
-
+                    clear();
+                    System.out.println(YELLOW + "Adding teacher..." + RESET);
+                    System.out.print("Enter Teacher first name: ");
+                    teacherFirstName = scanner.nextLine();
+                    System.out.print("Enter Teacher last name: ");
+                    teacherLastName = scanner.nextLine();
+                    System.out.print("Enter Teacher ID: ");
+                    teacherID = scanner.nextLine();
+                    //TODO: add teacher to database
+                    //temp actions-----------------
+                    Teacher newTEacher = new Teacher(teacherID, teacherFirstName, teacherLastName);
+                    //-----------------------------
+                    System.out.println(GREEN + "Teacher \"" + teacher.getFirstName() + " " + teacherLastName
+                            + "\" has been " +
+                            "successfully added");
                     isAdminActionChosen = true;
                     break;
                 case "8": //remove teacher
-                    System.out.println();
+                    clear();
+                    System.out.println(YELLOW + "Removing teacher..." + RESET);
+                    System.out.print("Enter Teacher ID: ");
+                    teacherID = scanner.nextLine();
+                    //TODO: find teacher with this ID and check if it exits
+                    //temp actions---------------------------
+                    teacher = null;
+                    //---------------------------------------
+                    System.out.println(GREEN + "Teacher with ID: " + teacherID +
+                            " has been successfully removed" + RESET);
                     isAdminActionChosen = true;
                     break;
                 case "9": //add course
-
+                    clear();
+                    System.out.println(YELLOW + "Adding course..." + RESET);
+                    System.out.print("Enter Course name: ");
+                    courseName = scanner.nextLine();
+                    System.out.print("Enter Course ID: ");
+                    courseID = scanner.nextLine();
+                    System.out.print("Enter Teacher ID: ");
+                    teacherID = scanner.nextLine();
+                    boolean isUnitInt = false;
+                    do {
+                        try {
+                            System.out.print("Enter number of units: ");
+                            numberOfUnits = scanner.nextInt();
+                            isUnitInt = true;
+                        } catch (InputMismatchException e) {
+                            System.out.println(RED + "Number of units must be an integer!" + RESET);
+                            scanner.nextLine(); //clear buffer
+                        }
+                    } while (!isUnitInt);
+                    boolean isExamDate = false;
+                    do {
+                        try {
+                            System.out.print("Enter exam date (Format: yyyy-mm-dd): ");
+                            courseExamDate = scanner.nextLine();
+                            dateOfExam = LocalDate.parse(courseExamDate);
+                            isExamDate = true;
+                        } catch (DateTimeParseException e) {
+                            System.out.println(RED + "Exam date format is not correct!" + RESET);
+                        }
+                    } while (!isExamDate);
+                    //TODO: validate teacherID, add course to database
+                    //temp actions-----------------
+                    Course newCourse = new Course(courseName, courseID, teacher, numberOfUnits, dateOfExam);
+                    //-----------------------------
+                    System.out.println(GREEN + "Course \"" + course.getName() + "\" has been " +
+                            "successfully added");
                     isAdminActionChosen = true;
                     break;
                 case "10": //remove course
-
+                    clear();
+                    System.out.println(YELLOW + "Removing course..." + RESET);
+                    System.out.print("Enter Course ID: ");
+                    courseID = scanner.nextLine();
+                    //TODO: find course with this ID and check if it exits
+                    //temp actions---------------------------
+                    course = null;
+                    //---------------------------------------
+                    System.out.println(GREEN + "Course with ID: " + courseID +
+                            " has been successfully removed" + RESET);
                     isAdminActionChosen = true;
                     break;
                 default:
@@ -212,8 +291,7 @@ public class Main {
                     break;
             }
         }
-
-
+        
         /*به نظرم بعد از انتخاب عملکرد ایدی استاد رو دریافت کنیم
         می تونیم بعد از انتخاب role استاد و قبل از پیام
         خوش آمد هم دریافت کنیم.*/
