8c3ca68690ed5ae84849d8430080f7ea664c9814
zahra azizi <azizi.zahra.tehran@gmail.com>
2024-06-27 11:09:50 +0330

    feat: new details for assignment (deadline, course, setters and getters)

--- src/classes/Assignment.java
+++ src/classes/Assignment.java
@@ -4,27 +4,50 @@ import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 
 public class Assignment {
-	private long deadline;
+
+	private long untilDeadline;
+	private LocalDate deadline;
 	private boolean isActive;
-	private String assignmentID;
+	private final String assignmentID;
+	private final Course course;
 
 
-	public Assignment(String assignmentID, LocalDate deadline, boolean isActive){
+	public Assignment(String assignmentID, LocalDate deadline, boolean isActive, Course course){
 		this.assignmentID = assignmentID;
-		this.deadline = ChronoUnit.DAYS.between(LocalDate.now(), deadline);
+		this.deadline = deadline;
+		this.untilDeadline = ChronoUnit.DAYS.between(LocalDate.now(), deadline);
 		this.isActive = isActive;
+		this.course = course;
 	}
 
 
-	public long getDeadline() {
+	public long getUntilDeadline() {
+		return untilDeadline;
+	}
+
+	public LocalDate getDeadline() {
 		return deadline;
+	}
+
+	public boolean isActive() {
+		return isActive;
 	}
 
 	public String getAssignmentID() {
 		return assignmentID;
 	}
 
+	public Course getCourse() {
+		return course;
+	}
+
+
 	public void setDeadline(LocalDate deadline) {
-		this.deadline = ChronoUnit.DAYS.between(LocalDate.now(), deadline);
+		this.deadline = deadline;
+		this.untilDeadline = ChronoUnit.DAYS.between(LocalDate.now(), deadline);
+	}
+
+	public void setActive(boolean active) {
+		isActive = active;
 	}
 }
