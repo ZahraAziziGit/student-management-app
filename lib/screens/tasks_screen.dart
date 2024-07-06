7807469350302e6383635ff81aa12563d8644779
zahra azizi <azizi.zahra.tehran@gmail.com>
2024-07-06 09:15:23 +0330

    feat: details

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 
-import 'package:radiohead/widgets/task_item.dart';
+import 'package:provider/provider.dart';
+
+import 'package:radiohead/widgets/task_provider.dart';
+import 'package:radiohead/widgets/task_item_with_time.dart';
 import 'package:radiohead/widgets/navigation_bar.dart';
 
 import 'package:radiohead/screens/home_screen.dart';
 
 class TasksScreen extends StatefulWidget {
-  const TasksScreen({Key? key}) : super(key: key);
+  const TasksScreen({super.key});
 
   @override
   State<TasksScreen> createState() => _TasksScreenState();
@@ -14,6 +17,8 @@ class TasksScreen extends StatefulWidget {
 
 class _TasksScreenState extends State<TasksScreen> {
   int _selectedIndex = 1;
+  DateTime _selectedDate = DateTime.now();
+  TimeOfDay _selectedTime = TimeOfDay.now();
 
   final List<Widget> _widgetOptions = <Widget>[
     const HomeContent(),
@@ -30,17 +35,101 @@ class _TasksScreenState extends State<TasksScreen> {
 
     if (index == 0) {
       Navigator.push(
-        context,
-        MaterialPageRoute(builder: (context) => const HomeScreen())
-      );
+          context, MaterialPageRoute(builder: (context) => const HomeScreen()));
     }
 
     if (index == 1) {
-      Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => const TasksScreen())
-      );
+      Navigator.push(context,
+          MaterialPageRoute(builder: (context) => const TasksScreen()));
     }
+  }
+
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2024),
+      lastDate: DateTime(2025),
+    );
+    if (picked != null) {
+      setState(() {
+        _selectedDate = picked;
+      });
+    }
+  }
+
+  Future<void> _selectTime(BuildContext context) async {
+    final TimeOfDay? picked = await showTimePicker(
+      context: context,
+      initialTime: TimeOfDay.now(),
+    );
+    if (picked != null) {
+      setState(() {
+        _selectedTime = picked;
+      });
+    }
+  }
+
+  void _showAddTaskDialog(BuildContext context) {
+    final taskNameController = TextEditingController();
+
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Add New Task'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                controller: taskNameController,
+                decoration: const InputDecoration(labelText: 'Task Name', border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(20)))),
+              ),
+              const SizedBox(height: 16),
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () => _selectDate(context),
+                      child: const Text('Date'),
+                    ),
+                    ElevatedButton(
+                      onPressed: () => _selectTime(context),
+                      child: const Text('Time'),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Icon (Icons.cancel_outlined),
+            ),
+            TextButton(
+              onPressed: () {
+                final name = taskNameController.text;
+                final deadline = DateTime(
+                  _selectedDate.year,
+                  _selectedDate.month,
+                  _selectedDate.day,
+                  _selectedTime.hour,
+                  _selectedTime.minute,
+                );
+                Provider.of<TaskProvider>(context, listen: false).addTask(name, deadline);
+                Navigator.of(context).pop();
+              },
+              child: const Icon (Icons.add_circle_outline),
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
@@ -49,7 +138,7 @@ class _TasksScreenState extends State<TasksScreen> {
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         elevation: 0,
-        leading: null,
+        leading: const Icon (Icons.remove, color: Colors.transparent),
       ),
       extendBodyBehindAppBar: true,
       body: Stack(
@@ -71,90 +160,67 @@ class _TasksScreenState extends State<TasksScreen> {
         currentIndex: _selectedIndex,
         onTap: _onItemTapped,
       ),
-    );
-  }
-}
-
-class TasksContent extends StatefulWidget {
-  const TasksContent({Key? key}) : super(key: key);
-
-  @override
-  _TasksContentState createState() => _TasksContentState();
-}
-
-class _TasksContentState extends State<TasksContent> {
-  final List<Task> _currentTasks = [
-    Task(name: 'ODE - HW1', completed: false),
-    Task(name: 'AP - HW2', completed: false),
-  ];
-
-  final List<Task> _doneTasks = [
-    Task(name: 'AP - HW1', completed: true),
-    Task(name: 'DM - HW1', completed: true),
-  ];
-
-  void _toggleTaskCompletion(Task task) {
-    setState(() {
-      if (task.completed) {
-        _doneTasks.remove(task);
-        _currentTasks.add(task);
-      } else {
-        _currentTasks.remove(task);
-        _doneTasks.add(task);
-      }
-      task.completed = !task.completed;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: ListView(
-        children: [
-          const SizedBox(height: 18),
-          const Text(
-            'Current tasks',
-            textAlign: TextAlign.left,
-            style: TextStyle(
-                fontSize: 18, fontWeight: FontWeight.bold, color: Colors.white),
-          ),
-          const SizedBox(height: 12),
-          Column(
-            children: _currentTasks
-                .map((task) => TaskItem(
-              taskName: task.name,
-              initialCompleted: task.completed,
-              onToggleCompletion: () => _toggleTaskCompletion(task),
-            ))
-                .toList(),
-          ),
-          const SizedBox(height: 12),
-          const Text(
-            'Done tasks',
-            textAlign: TextAlign.left,
-            style: TextStyle(
-                fontSize: 18, fontWeight: FontWeight.bold, color: Colors.white),
-          ),
-          const SizedBox(height: 12),
-          Column(
-            children: _doneTasks
-                .map((task) => TaskItem(
-              taskName: task.name,
-              initialCompleted: task.completed,
-              onToggleCompletion: () => _toggleTaskCompletion(task),
-            ))
-                .toList(),
-          ),
-        ],
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _showAddTaskDialog(context),
+        child: Icon(Icons.add),
       ),
     );
   }
 }
 
-class Task {
-  String name;
-  bool completed;
+class TasksContent extends StatelessWidget {
+  const TasksContent({super.key});
 
-  Task({required this.name, this.completed = false});
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<TaskProvider>(
+      builder: (context, taskProvider, child) {
+        return Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: ListView(
+            children: [
+              const SizedBox(height: 18),
+              const Text(
+                'Current tasks',
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                    fontSize: 18, fontWeight: FontWeight.bold, color: Colors.white),
+              ),
+              const SizedBox(height: 12),
+              Column(
+                children: taskProvider.currentTasks
+                    .map((task) => TaskItemWithTime(
+                  taskName: task.name,
+                  initialCompleted: task.completed,
+                  onToggleCompletion: () => taskProvider.toggleTaskCompletion(task),
+                  deadline: task.deadline,
+                  onToggleDelete: () => taskProvider.deleteTask(task),
+                ))
+                    .toList(),
+              ),
+              const SizedBox(height: 12),
+              const Text(
+                'Done tasks',
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                    fontSize: 18, fontWeight: FontWeight.bold, color: Colors.white),
+              ),
+              const SizedBox(height: 12),
+              Column(
+                children: taskProvider.doneTasks
+                    .map((task) => TaskItemWithTime(
+                  taskName: task.name,
+                  initialCompleted: task.completed,
+                  onToggleCompletion: () => taskProvider.toggleTaskCompletion(task),
+                  deadline: task.deadline,
+                  onToggleDelete: () => taskProvider.deleteTask(task),
+                ))
+                    .toList(),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
 }
