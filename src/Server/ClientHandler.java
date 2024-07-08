f3c208b4369cda4ab46000d47c6b5515196475ee
zahra azizi <azizi.zahra.tehran@gmail.com>
2024-07-09 00:57:17 +0330

    feat: cases for frontend request (sign up, login, userinfo -initail request)

--- src/Server/ClientHandler.java
+++ src/Server/ClientHandler.java
@@ -18,18 +18,13 @@ class ClientHandler extends Thread {
     public String listener() throws IOException {
         try {
             System.out.println("Listener started");
-            StringBuilder sb = new StringBuilder();
+            StringBuilder request = new StringBuilder();
             int index = dis.read();
             while (index != 0) {
-                sb.append((char) index);
+                request.append((char) index);
                 index = dis.read();
             }
-            Scanner s = new Scanner(sb.toString());
-            StringBuilder request = new StringBuilder();
-            while (s.hasNextLine()) {
-                request.append(s.nextLine());
-            }
-            System.out.println("listener has read command successfully: { " + request.toString() + " }");
+            System.out.println("listener has read command successfully: { " + request + " }");
             return request.toString();
         } catch (IOException e) {
             System.out.println("Error in listener : " + e.getMessage());
@@ -38,7 +33,7 @@ class ClientHandler extends Thread {
     }
 
     public void writer(String write) throws IOException {
-        try{
+        try {
             System.out.println("Writer started");
             dos.writeBytes(write);
             dos.flush();
@@ -56,20 +51,10 @@ class ClientHandler extends Thread {
         super.run();
         String command;
         try {
-            command = listener();
-            String[] studentData = command.split("~");
-            for (String s : studentData)
-                System.out.println(s);
-
-            String studentUsername = studentData[0];
-            String studentId = studentData[1];
-            String studentPassword = studentData[2];
 
             //database directory
             String filePath = "./Database";
             //String filePath = ".\\Database"; //for windows, uncomment this and comment the previous line.
-            File tempFile = new File(filePath);
-            tempFile.mkdir();
 
             String studentPath = filePath + "/students_data.txt";
             String assignmentPath = filePath + "/assignments_data.txt";
@@ -81,7 +66,8 @@ class ClientHandler extends Thread {
         /*  String studentPath = filePath + "\\students_data.txt";
             String assignmentPath = filePath + "\\assignments_data.txt";
             String teacherPath = filePath + "\\teachers_data.txt";
-            String coursePath = filePath + "\\courses_data.txt"; */
+            String coursePath = filePath + "\\courses_data.txt";
+             String userPath = filePath + "\\users_data.txt"; */
 
             File studentFile = new File(studentPath);
             File assignmentFile = new File(assignmentPath);
@@ -95,24 +81,129 @@ class ClientHandler extends Thread {
                 System.out.println(e.getMessage());
             }
 
-            Scanner reader = new Scanner(studentFile);
-            String studentDataFromDB = "";
-            boolean isStudentFound = false;
-            while (reader.hasNext()) {
-                studentDataFromDB = reader.nextLine();
-                String[] detail = studentDataFromDB.split(",");
-                if (detail[2].split(":")[1].equals(studentId))
-                    isStudentFound = true;
+            String[] userData;
+            String studentUsername;
+            String studentId;
+            String studentPassword;
+            String studentUsernameOrId = "";
+
+            String studentDataFromDB;
+            String[] studentDetailsFromDB;
+
+            command = listener();
+
+            String action = command.split("~")[0];
+            switch (action) {
+
+                case "signup":
+                    userData = command.split("~");   //example: signup~testUser~123456789~password
+                    studentUsername= userData[1];
+                    studentId = userData[2];
+                    studentPassword = userData[3];
+
+                    boolean isUsernameDuplicate = false;
+                    Scanner userReader = new Scanner(userFile);
+                    while (userReader.hasNext()) {
+                        String[] userDataFromDB = userReader.nextLine().split("~");
+                        String username = userDataFromDB[0].split(":")[1];
+                        if (username.equals(studentUsername)) {
+                            isUsernameDuplicate = true;
+                            break;
+                        }
+                    }
+
+                    if (!isUsernameDuplicate) {
+                        Scanner studentReader = new Scanner(studentFile);
+                        boolean isStudentFound = false;
+                        while (studentReader.hasNext()) {
+                            studentDataFromDB = studentReader.nextLine();
+                            studentDetailsFromDB = studentDataFromDB.split(",");
+                            if (studentDetailsFromDB[2].split(":")[1].equals(studentId))
+                                isStudentFound = true;
+                        }
+                        if (isStudentFound) {
+                            writer("found");
+                            Writer writer = new FileWriter(userFile, true);
+                            writer.write("username:" + studentUsername+ "~id:" + studentId + "~password:" + studentPassword + "\n");
+                            writer.flush();
+                            writer.close();
+                        } else
+                            writer("not found");
+                        break;
+                    } else {
+                        writer("duplicate");
+                    }
+                    break;
+
+                case "login":
+                    String[] studentData = command.split("~");   //example: login~testUser~password
+                    studentUsernameOrId = studentData[1];
+                    studentPassword = studentData[2];
+
+                    boolean isUserFound = false;
+                    boolean isPasswordOk = false;
+                    userReader = new Scanner(userFile);
+                    while (userReader.hasNext()) {
+                        String[] userDataFromDB = userReader.nextLine().split("~");
+                        String username = userDataFromDB[0].split(":")[1];
+                        String id = userDataFromDB[1].split(":")[1];
+                        String password = userDataFromDB[2].split(":")[1];
+                        if (username.equals(studentUsernameOrId) || id.equals(studentUsernameOrId)) {
+                            isUserFound = true;
+                            if (password.equals(studentPassword)) {
+                                isPasswordOk = true;
+                            }
+                            break;
+                        }
+                    }
+
+                    if (isUserFound) {
+                        if (isPasswordOk) {
+                            writer("found");
+                        } else
+                            writer("wrong password");
+                        break;
+                    } else {
+                        writer("not found");
+                    }
+                    break;
+
+                case "userinfo":
+
+                    //userinfo
+                    String name = "", lastname = "", id = "", unitCounts = "", totalAvg = "";
+
+                    userReader = new Scanner(userFile);
+                    while (userReader.hasNext()) {
+                        String[] userDataFromDB = userReader.nextLine().split("~");
+                        String username = userDataFromDB[0].split(":")[1];
+                        id = userDataFromDB[1].split(":")[1];
+                        if (username.equals(studentUsernameOrId) || id.equals(studentUsernameOrId))
+                            break;
+                    }
+
+                    Scanner studentReader = new Scanner(studentFile);
+                    while (studentReader.hasNext()) {
+                        studentDataFromDB = studentReader.nextLine();
+                        studentDetailsFromDB = studentDataFromDB.split(",");
+                        if (studentDetailsFromDB[2].split(":")[1].equals(id)) {
+                            name = studentDetailsFromDB[0].split(":")[1];
+                            lastname = studentDetailsFromDB[1].split(":")[1];
+                            id = studentDetailsFromDB[2].split(":")[1];
+                            unitCounts = studentDetailsFromDB[4].split(":")[1];
+                            totalAvg = studentDetailsFromDB[6].split(":")[1];
+                            break;
+                        }
+                    }
+                    //name~lastname~id~unitCount~avg
+                    writer(name + "~" + lastname + "~" + id + "~" + unitCounts + "~" + totalAvg);
+
+                    break;
+
+
             }
-            if (isStudentFound) {
-                writer("found");
-                Writer writer = new FileWriter(userFile);
-                writer.write("username:" + studentUsername + "~id:" + studentId + "~password:" + studentPassword + "\n");
-                writer.flush();
-                writer.close();
-            }
-            else
-                writer("not found");
+
+
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
