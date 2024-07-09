6dd59e7beb8970a3caf29beacddbe6fa8f795a08
zahra azizi <azizi.zahra.tehran@gmail.com>
2024-07-10 00:52:28 +0330

    chore: test and check details for admin action 16

--- src/Database/StoreData.java
+++ src/Database/StoreData.java
@@ -19,18 +19,27 @@ public class StoreData {
             for (String courseId : listOfCourse) {
                 coursesId.append(courseId).append("~");
             }
-            if (!coursesId.isEmpty())
-                writer.write(coursesId.substring(0, coursesId.length() - 1) + "},totalAvg:" + totalAvg + ",marks:{");
-            else
+            if (!coursesId.isEmpty()) {
+                if (coursesId.charAt(0) == '~') {
+                    writer.write(coursesId.substring(1, coursesId.length() - 1) + "},totalAvg:" + totalAvg + ",marks:{");
+                } else {
+                    writer.write(coursesId.substring(0, coursesId.length() - 1) + "},totalAvg:" + totalAvg + ",marks:{");
+                }
+            } else
                 writer.write("},totalAvg:" + totalAvg + ",marks:{");
 
             StringBuilder marksStr = new StringBuilder();
             for (String courseId : marks.keySet()) {
                 marksStr.append(courseId).append("=").append(marks.get(courseId)).append("~");
             }
-            if (!marksStr.isEmpty())
-                writer.write(marksStr.substring(0, marksStr.length() - 1) + "}\n");
-            else
+            if (!marksStr.isEmpty()) {
+                if (marksStr.charAt(0) == '~') {
+                    writer.write(marksStr.substring(1, marksStr.length() - 1) + "}\n");
+                } else {
+                    writer.write(marksStr.substring(0, marksStr.length() - 1) + "}\n");
+
+                }
+            } else
                 writer.write("}\n");
 
             writer.flush();
@@ -40,7 +49,7 @@ public class StoreData {
         }
     }
 
-    public static void storeAssignment(String id, String deadline, boolean isActive, String courseId,String name, File file) {
+    public static void storeAssignment(String id, String deadline, boolean isActive, String courseId, String name, File file) {
         try {
             FileWriter writer = new FileWriter(file, true);
             writer.write("id:" + id + ",deadline:" + deadline + ",active:" + isActive + ",course:" + courseId + ",name:" + name + "\n");
@@ -61,9 +70,13 @@ public class StoreData {
             for (String courseId : listOfCourse) {
                 coursesId.append(courseId).append("~");
             }
-            if (!coursesId.isEmpty())
-                writer.write(coursesId.substring(0, coursesId.length() - 1) + "}\n");
-            else
+            if (!coursesId.isEmpty()) {
+                if (coursesId.charAt(0) == '~') {
+                    writer.write(coursesId.substring(1, coursesId.length() - 1) + "}\n");
+                } else {
+                    writer.write(coursesId.substring(0, coursesId.length() - 1) + "}\n");
+                }
+            } else
                 writer.write("}\n");
 
             writer.flush();
@@ -77,7 +90,7 @@ public class StoreData {
                                    String teacherId, Map<String, Double> marks, int numOfUnits,
                                    int numOfStudents, List<String> listOfStudents, boolean isActive,
                                    int numOfAssignments, List<String> listOfAssignments,
-                                   String examDate,  File file) {
+                                   String examDate, File file) {
         try {
             FileWriter writer = new FileWriter(file, true);
             writer.write("name:" + name + ",id:" + id + ",teacherId:" + teacherId + ",marks:<");
@@ -86,29 +99,42 @@ public class StoreData {
             for (String studentId : marks.keySet()) {
                 marksStr.append(studentId).append("#").append(marks.get(studentId)).append("*");
             }
-            if (!marksStr.isEmpty())
-                writer.write(marksStr.substring(0, marksStr.length() - 1) + ">,numOfUnits:" + numOfUnits +
-                         ",numOfStudents:" + numOfStudents + ",students:{");
-            else
+            if (!marksStr.isEmpty()) {
+                if (marksStr.charAt(0) == '*') {
+                    writer.write(marksStr.substring(1, marksStr.length() - 1) + ">,numOfUnits:" + numOfUnits +
+                            ",numOfStudents:" + numOfStudents + ",students:{");
+                } else {
+                    writer.write(marksStr.substring(0, marksStr.length() - 1) + ">,numOfUnits:" + numOfUnits +
+                            ",numOfStudents:" + numOfStudents + ",students:{");
+                }
+            } else
                 writer.write(">,numOfUnits:" + numOfUnits + ",numOfStudents:" + numOfStudents + ",students:{");
 
             StringBuilder studentsId = new StringBuilder();
             for (String studentId : listOfStudents) {
                 studentsId.append(studentId).append("~");
             }
-            if (!studentsId.isEmpty())
-                writer.write(studentsId.substring(0, studentsId.length() - 1) + "},active:" + isActive +
-                        ",numOfAssignments:" + numOfAssignments + ",assignments:{");
-            else
+            if (!studentsId.isEmpty()) {
+                if (studentsId.charAt(0) == '~') {
+                    writer.write(studentsId.substring(1, studentsId.length() - 1) + "},active:" + isActive +
+                            ",numOfAssignments:" + numOfAssignments + ",assignments:{");
+                } else {
+                    writer.write(studentsId.substring(0, studentsId.length() - 1) + "},active:" + isActive +
+                            ",numOfAssignments:" + numOfAssignments + ",assignments:{");
+                }
+            } else
                 writer.write("},active:" + isActive + ",numOfAssignments:" + numOfAssignments + ",assignments:{");
 
             StringBuilder assignmentsId = new StringBuilder();
             for (String assignmentId : listOfAssignments) {
                 assignmentsId.append(assignmentId).append("~");
             }
-            if (!assignmentsId.isEmpty())
-                writer.write(assignmentsId.substring(0, assignmentsId.length() - 1) + "},examDate:" + examDate + "\n");
-            else
+            if (!assignmentsId.isEmpty()) {
+                if (assignmentsId.charAt(0) == '~') {
+                    writer.write(assignmentsId.substring(1, assignmentsId.length() - 1) + "},examDate:" + examDate + "\n");
+                }else {
+                    writer.write(assignmentsId.substring(0, assignmentsId.length() - 1) + "},examDate:" + examDate + "\n");
+                }            } else
                 writer.write("},examDate:" + examDate + "\n");
 
             writer.flush();
