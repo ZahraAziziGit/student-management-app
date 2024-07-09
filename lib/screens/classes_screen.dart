e4a042d4aae8f16f0eea2da523dbd509a4995879
zahra azizi <azizi.zahra.tehran@gmail.com>
2024-07-09 20:18:10 +0330

    classes connect to backend

--- lib/screens/classes_screen.dart
+++ lib/screens/classes_screen.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:radiohead/widgets/course_provider.dart';
 
 import 'package:radiohead/widgets/navigation_bar.dart';
 import 'package:radiohead/widgets/class_card.dart';
@@ -27,6 +31,10 @@ class _ClassesScreenState extends State<ClassesScreen> {
   ];
 
   void _onItemTapped(int index) {
+    if (index != _selectedIndex) {
+      Provider.of<CourseProvider>(context, listen: false).clearCourses();
+    }
+
     setState(() {
       _selectedIndex = index;
     });
@@ -47,8 +55,8 @@ class _ClassesScreenState extends State<ClassesScreen> {
     }
 
     if (index == 3) {
-      Navigator.push(context,
-          MaterialPageRoute(builder: (context) => const NewsScreen()));
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => const NewsScreen()));
     }
 
     if (index == 4) {
@@ -56,46 +64,6 @@ class _ClassesScreenState extends State<ClassesScreen> {
           MaterialPageRoute(builder: (context) => const AssignmentsScreen()));
     }
   }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        leading: const Icon(Icons.remove, color: Colors.transparent),
-      ),
-      extendBodyBehindAppBar: true,
-      body: Stack(
-        children: [
-          Container(
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage("assets/images/purple-pattern.jpg"),
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
-          // Content
-          _widgetOptions[_selectedIndex],
-        ],
-      ),
-      bottomNavigationBar: CustomBottomNavigationBar(
-        currentIndex: _selectedIndex,
-        onTap: _onItemTapped,
-      ),
-    );
-  }
-}
-
-class ClassesContent extends StatefulWidget {
-  const ClassesContent({super.key});
-
-  @override
-  ClassesContentState createState() => ClassesContentState();
-}
-
-class ClassesContentState extends State<ClassesContent> {
 
   void _addNewClass(BuildContext context) {
     showModalBottomSheet(
@@ -157,55 +125,143 @@ class ClassesContentState extends State<ClassesContent> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(20.0),
-      child: Column(
-        children: [
-          const SizedBox(height: 25),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              const Text("Classes",
-                  style: TextStyle(color: Colors.white, fontSize: 36)),
-              IconButton(
-                icon: const Icon(Icons.library_add,
-                    color: Colors.white, size: 36),
-                onPressed: () => _addNewClass(context),
-              ),
-            ],
-          ),
-          const SizedBox(height: 12),
-          const Expanded(
-            child: SingleChildScrollView(
-              child: Column(
-                children: [
-                  ClassCard(
-                    className: "Advanced Programming",
-                    teacherName: "Dr. Vahidi",
-                    numOfUnits: 3,
-                    numOfAssignments: 4,
-                    bestStudentName: "Taghi Taghavi",
-                  ),
-                  ClassCard(
-                    className: "Advanced Programming",
-                    teacherName: "Dr. Vahidi",
-                    numOfUnits: 3,
-                    numOfAssignments: 4,
-                    bestStudentName: "Taghi Taghavi",
-                  ),
-                  ClassCard(
-                    className: "Advanced Programming",
-                    teacherName: "Dr. Vahidi",
-                    numOfUnits: 3,
-                    numOfAssignments: 4,
-                    bestStudentName: "Taghi Taghavi",
-                  ),
-                ],
-              ),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: const Icon(Icons.remove, color: Colors.transparent),
+        actions: [
+          IconButton(
+            icon: const Padding(
+              padding: EdgeInsets.all(18.0),
+              child:
+                  Icon(Icons.add_box_outlined, color: Colors.white, size: 30),
             ),
+            onPressed: () => _addNewClass(context),
           ),
         ],
       ),
+      extendBodyBehindAppBar: true,
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/images/purple-pattern.jpg"),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          // Content
+          _widgetOptions[_selectedIndex],
+        ],
+      ),
+      bottomNavigationBar: CustomBottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+      ),
     );
+  }
+}
+
+class ClassesContent extends StatefulWidget {
+  const ClassesContent({super.key});
+
+  @override
+  ClassesContentState createState() => ClassesContentState();
+}
+
+class ClassesContentState extends State<ClassesContent> {
+  String response = "";
+  String? courseName, teacherName, units, assignments, topStudent;
+
+  @override
+  void initState() {
+    super.initState();
+    _initializeClasses();
+  }
+
+  Future<void> _initializeClasses() async {
+    await classes();
+    _removeHalfCourses();
+  }
+
+  void _removeHalfCourses() {
+    int halfCount = (Provider.of<CourseProvider>(context, listen: false).getCourseCount() / 3).floor();
+    Provider.of<CourseProvider>(context, listen: false).removeCourses(halfCount);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<CourseProvider>(
+      builder: (context, courseProvider, child) {
+        return Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: ListView(
+            children: [
+              const SizedBox(height: 18),
+              Column(
+                children: courseProvider.courses.map((course) {
+                  return SizedBox(
+                    child: ClassCard(
+                      className: course.courseName,
+                      teacherName: course.teacherName,
+                      numOfUnits: course.numOfUnits,
+                      numOfAssignments: course.numOfAssignments,
+                      bestStudentName: course.bestStudentName,
+                    ),
+                  );
+                }).toList(),
+              ),
+              const SizedBox(
+                height: 12,
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  Future<String> classes() async {
+    try {
+      final serverSocket = await Socket.connect("192.168.1.102", 2041);
+
+      serverSocket.write('classes\u0000');
+      serverSocket.flush();
+
+      serverSocket.listen((socketResponse) {
+        setState(() {
+          response = String.fromCharCodes(socketResponse);
+          print("-----------server response is: { $response }");
+
+          List<String> data = response.split("#");
+          for (int i = 0; i < data.length - 1; i++) {
+            print("-----------details are: { ${data[i]} }");
+            courseName = data[i].split("~")[0];
+            teacherName = data[i].split("~")[1];
+            units = data[i].split("~")[2];
+            assignments = data[i].split("~")[3];
+            topStudent = data[i].split("~")[4];
+            Provider.of<CourseProvider>(context, listen: false).addCourse(
+              courseName!,
+              teacherName!,
+              int.parse(units!),
+              int.parse(assignments!),
+              topStudent!,
+            );
+          }
+        });
+      });
+
+      await Future.delayed(const Duration(seconds: 1));
+    } catch (e) {
+      print("Error: $e");
+      if (e is SocketException) {
+        print(
+            'SocketException: ${e.message}, address: ${e.address}, port: ${e.port}');
+      }
+    }
+    return response;
   }
 }
